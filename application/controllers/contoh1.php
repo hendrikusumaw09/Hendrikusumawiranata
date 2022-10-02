4b220590723b96bb7cf6bb56cf5f56fa9ddf4227
hendrikusumaw09 <hendrikusumawiranata01@gmail.com>
2022-10-02 17:05:27 +0700

    tugas pertemuan 1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+     {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Hendri kusuma wiranata
+            Saya tingga di daerah desa kapur olahraga yang saya sukai adalah bola volly";
+     }
+}
