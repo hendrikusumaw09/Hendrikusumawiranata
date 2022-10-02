4cc32d954eeec06a011a9edcf286ee820206331b
hendrikusumaw09 <hendrikusumawiranata01@gmail.com>
2022-10-02 19:09:56 +0700

    tugas pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+     <h1><?php echo $judul ?></h1
+     <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Hendri kusuma</li>
+        <li>Nama Belakang : wiranata</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Kapur,kec,sungai raya,kab kubu raya</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>kambong</li>
+    </ul>
+    
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>bola volly</li>
+        <li>catur</li>
+    </ul>
+</section>
